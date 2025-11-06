f4598348e0450da2a18eda1939a282647885e775
HarshalTe <harshalterekar509@gmail.com>
2025-11-06 00:34:17 -0800

    push1

--- src/Hmain.js
+++ src/Hmain.js
@@ -4,54 +4,26 @@ import Dropdown1 from "./Dropdown1";
 import DynamicForm from "./DynamicForm";
 import H1 from "./H1";
 import H2 from './H2'
-const Hmain = ()=>{
-     return(
-        <>
-          {/* <H1/> */}
-          <h1>33333</h1>
-          {/* <H2/> */}
-          {/* <CompanyWorkDropdown/> */}
-          {/* <Dropdown1/> */}
-          {/* <DynamicForm/> */}
+const Hmain = () => {
+  return (
+    <>
+      {/* <H1/> */}
 
-      <h1>start</h1>
-        <h1>start</h1>
-        <h1>start</h1>
-        <h1>start</h1>
-        <h1>123654</h1>
-             <h1>123654</h1>
-          <Da val={20}  nnnn={256}/>
+      {/* <H2/> */}
+      {/* <CompanyWorkDropdown/> */}
+      {/* <Dropdown1/> */}
+      {/* <DynamicForm/> */}
+
+
+      <Da />
 
 
 
-          <Da val={20} na={"harshal"} nw={"somthing...."} fullname={"hbt"} ne={"ne"}/>
-
-          <h1>HARSHA</h1>
-      <h1>s</h1>
-      <h1>s</h1>
-
-          <h1>HARSHAL</h1>
-            <h1>hhhhh</h1>
 
 
-          <h2>PRATHAMESH</h2>
 
-            <h1>HARSHAL2</h1>
-                  <h1>HARSHAL</h1>
 
-          <h2>PRATHAMESH</h2>
-
-            <h1>HARSHAL2</h1>
-                  <h1>HARSHAL</h1>
-
-          <h2>PRATHAMESH</h2>
-
-            <h1>HARSHAL2</h1>
-     <Da/>
-          <Da/>
-               <Da/>
-
-        </>
-     );
+    </>
+  );
 }
 export default Hmain;
